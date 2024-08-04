aa8a0d89ef2b92ded7baa2fdae403e3e2a729814
Neville Mitshiabu <agencewonorogo@gmail.com>
2024-08-04 12:39:39 +0100

    Affichage des par ordre : Abonnement et Pour vous

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+// function showTab(tabName) {
+//   // Remove 'active' class from all tab contents and tabs
+//   document.querySelectorAll('.tab-content').forEach(el => el.classList.remove('active'));
+//   document.querySelectorAll('.page-tab').forEach(el => el.classList.remove('active'));
+
+//   // Add 'active' class to the selected tab content and tab
+//   document.getElementById(`content-${tabName}`).classList.add('active');
+//   document.getElementById(`tab-${tabName}`).classList.add('active');
+// }
+
+// // Initialize with the default tab
+// document.addEventListener('DOMContentLoaded', () => {
+//   showTab('all'); // Default tab
+// });
